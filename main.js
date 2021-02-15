05bbfc859440c06e7743a56c1b9480e931cc8ae6
vishweshkadam <75071313+vishweshkadam@users.noreply.github.com>
2021-02-15 17:59:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var menu_list_array=
+["Chicken Tandoori Pizza",
+ "Paneer Tikka Pizza", 
+ "Pepperoni Pizza",
+ "Pasta Pizza",
+ "Veg Deluxe Pizza"];
+
+function getmenu(){
+    var htmldata;
+    htmldata="<ol class='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<li>'+menu_list_array[i]+'</li>';
+    }
+    htmldata=htmldata+"</ol>";
+    document.getElementById("show_menu").innerHTML=htmldata;
+}
+
+function add_item(){
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>";
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<div class="card">'+'<img src="pizzaImg.png"/>'+menu_list_array[i]+'</div>'
+    }
+    htmldata=htmldata+"</section>"
+    document.getElementById("display_addedmenu").innerHTML=htmldata;
+    
+}
